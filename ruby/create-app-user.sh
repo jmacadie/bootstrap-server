1b771712cd3d1daa438d7ebf4b037a1d313a587f
James MacAdie (server name) <james@macadie.co.uk>
2015-08-26 16:14:11 +0100

    Move config files to new user
    Make sure the experience of the new user isn't totally crap

--- ruby/create-app-user.sh
+++ ruby/create-app-user.sh
@@ -13,3 +13,12 @@ sudo sh -c "cat $HOME/.ssh/id_rsa >> ~$uname/.ssh/id_rsa"
 sudo sh -c "chown -R $uname: ~$uname/.ssh"
 sudo sh -c "chmod 700 ~$uname/.ssh"
 sudo sh -c "chmod 600 ~$uname/.ssh/*"
+
+# Move config files (so experience is the same)
+sudo sh -c "cp -r ../.bash/ ~$uname/.bash/"
+
+sudo sh -c "rm -f ~$uname/.bashrc"
+sudo sh -c "cp -r ../.bashrc ~$uname/.bashrc"
+
+sudo sh -c "rm -f ~$uname/.vimrc"
+sudo sh -c "cp -r ../.vimrc ~$uname/.vimrc"
