cc2c76f654cb8b033833d2d0f12582082f8c2d2a
James MacAdie (server name) <james@macadie.co.uk>
2015-08-28 14:20:21 +0100

    Fix to wipe-app script

--- ruby/wipe-app.sh
+++ ruby/wipe-app.sh
@@ -3,6 +3,7 @@ read APP_NAME
 echo "Removing $APP_NAME ..."
 
 sudo userdel $APP_NAME
-sudo rm -f /etc/ngingx/sites-enabled/$APP_NAME.conf
-sudo rm -f /etc/ngingx/sites-available/$APP_NAME.conf
+sudo rm -rf /home/$APP_NAME
+sudo sh -c "rm -f /etc/ngingx/sites-enabled/$APP_NAME.conf"
+sudo sh -c "rm -f /etc/ngingx/sites-available/$APP_NAME.conf"
 sudo rm -rf /var/www/$APP_NAME
