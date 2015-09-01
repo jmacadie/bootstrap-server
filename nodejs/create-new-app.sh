5f54e09eea4088849603158ec9d90708abf692e8
James MacAdie (server name) <james@macadie.co.uk>
2015-09-01 17:28:13 +0100

    Added Node.js set up scripts
    Using Express & handlebars templating

---
+++ nodejs/create-new-app.sh
@@ -0,0 +1,144 @@
+#!bin/bash
+
+echo -n "Provide an app name > "
+read APP_NAME
+echo "Adding $APP_NAME ..."
+
+echo -n "Provide a server name > "
+read SERVER
+
+sudo adduser $APP_NAME
+
+# Ensure inbound and outbound SSH keys are installed as per current user
+sudo sh -c "mkdir -p ~$APP_NAME/.ssh"
+sudo sh -c "cat $HOME/.ssh/authorized_keys >> ~$APP_NAME/.ssh/authorized_keys"
+sudo sh -c "cat $HOME/.ssh/id_rsa >> ~$APP_NAME/.ssh/id_rsa"
+sudo sh -c "chown -R $APP_NAME: ~$APP_NAME/.ssh"
+sudo sh -c "chmod 700 ~$APP_NAME/.ssh"
+sudo sh -c "chmod 600 ~$APP_NAME/.ssh/*"
+
+# Move config files (so experience is the same)
+sudo sh -c "cp -rf $HOME/.bash/ ~$APP_NAME/.bash/"
+sudo sh -c "cp -rf $HOME/.bashrc ~$APP_NAME/.bashrc"
+sudo sh -c "cp -rf $HOME/.vimrc ~$APP_NAME/.vimrc"
+
+# Create a shell folder for the project
+sudo mkdir -p /var/www/$APP_NAME
+cd /var/www/$APP_NAME
+mkdir site
+cd site
+
+# Create some folders & files we'll need
+mkdir views
+mkdir views/layouts
+mkdir views/partials
+mkdir public
+mkdir public/css
+mkdir public/img
+mkdir public/js
+mkdir public/qa
+mkdir public/vendor
+mkdir lib
+touch credentials.js
+
+# Create a .gitignore
+cat <<EOF .gitignore
+# Ignore packages installed by npm
+node_modules*
+
+# Don't store credentials in repo
+credentials.js
+
+# Don't commit log files
+log
+
+# Don't store ssl certs in repo
+ssl
+EOF
+
+# Initialse the project and install express & handlebars
+npm init
+npm install --save express
+npm install --save express-handlebars
+
+# Write a very basic app.js main file
+cat <<EOF app.js
+var express = require('express');
+var app = express();
+
+// Set up handlebars view engine
+var handlebars = require('express-handlebars')
+                    .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set(port, process.env.PORT || 3000);
+
+// Routes
+app.get('/', function (req, res){
+  res.render('home');
+});
+
+// Custom 404 page
+app.use(function(req, res){
+  res.status(404);
+  res.render('404');
+});
+
+// Custom 500 page
+app.use(function(req, res){
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express Started on http://localhost:' + app.get('port') +
+              '; Press Ctrl + c to terminate.');
+});
+EOF
+
+# Write a basic layout file
+cat <<EOF views/layouts/main.handlebars
+<!doctype html>
+<head>
+  <title>$APP_NAME</title>
+</head>
+<body>
+  {{{body}}}
+</body>
+</html>
+EOF
+
+# Write some basic views
+echo "Hello, World!" >> views/home.handlebars
+echo "404 - Not Found" >> views/404.handlebars
+echo "500 - Server Error" >> views/500.handlebars
+
+# Chnage the folder permissions
+sudo sh -c "chown -R $APP_NAME: /var/www/$APP_NAME"
+
+# Set up virtual host
+ROOT_PATH=/var/www/$APP_NAME/site/public
+sudo tee /etc/nginx/sites-available/$APP_NAME.conf >/dev/null <<EOF
+server {
+  listen 80;
+  server_name $SERVER;
+
+  # Tell Nginx and Passenger where your app's 'public' directory is
+  root $ROOT_PATH;
+
+  # Turn on Passenger
+  passenger_enabled on;
+  passenger_app_type node;
+  passenger_startup_file app.js;
+}
+EOF
+sudo ln -s \
+/etc/nginx/sites-available/$APP_NAME.conf \
+/etc/nginx/sites-enabled/$APP_NAME.conf
+
+# Restart Nginx
+sudo service nginx restart
+
+# Test to see if it worked
+#curl http://$SERVER
---
+++ nodejs/node-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Install Node from the NodeSource APT repository
+sudo apt-get update
+sudo apt-get install -y curl apt-transport-https ca-certificates &&
+  curl --fail -ssL -o setup-nodejs https://deb.nodesource.com/setup_0.12 &&
+  sudo bash setup-nodejs &&
+  sudo apt-get install -y nodejs build-essential
+
+# Install Phusion PGP key and add HTTPS support for APT
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+
+# Add Phusion APT repository
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger jessie main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+
+# Install Passenger + Nginx
+sudo apt-get install -y nginx-extras passenger
+
+# Enable Passenger Nginx module
+sudo sed -i "s/# passenger_root/passenger_root/g" /etc/nginx/nginx.conf
+sudo sed -i "s/# passenger_ruby/passenger_ruby/g" /etc/nginx/nginx.conf
+
+# Restart Nginx
+sudo service nginx restart
+
+# Check it went OK
+echo Checking everything intsalled correctly
+sudo passenger-config validate-install
+sudo passenger-memory-stats
