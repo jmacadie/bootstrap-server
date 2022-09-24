c83194f4de35a86b4cd04a28f56520620b539898
James MacAdie (server name) <james@macadie.co.uk>
2022-09-24 17:09:48 +0100

    Add required dependency
    The PG gem needs libpq-dev

--- ruby/postgres-setup.sh
+++ ruby/postgres-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #https://www.digitalocean.com/community/tutorials/how-to-install-postgresql-on-ubuntu-22-04-quickstart
 
-sudo apt-get install postgresql postgresql-contrib
+sudo apt-get install postgresql postgresql-contrib libpq-dev
 
 # Create role for current user (replace X), need a DB with the same name
 sudo -u postgres createuser -s $USER
