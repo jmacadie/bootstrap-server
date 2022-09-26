0300f422f79d6711e993b9f7748c7ea6b2816a28
James MacAdie (server name) <james@macadie.co.uk>
2022-09-26 16:28:14 +0100

    Postgres updates
    Some updates to the scripts to better work with postgres, now that I've
    tested it through more thoroughly

--- ruby/create-new-app.sh
+++ ruby/create-new-app.sh
@@ -22,7 +22,7 @@ sudo tee $ROOT_PATH/puma.rb >/dev/null <<EOF
 ENV['APP_ENV'] = 'production'
 
 #ruby -e "require 'sysrandom/securerandom'; puts SecureRandom.hex(64)"
-ENV['SESSION_SECRET'] = '<REPLACE_ME>'
+ENV['SESSION_SECRET'] = '<REPLACE_ME.................................................PADDING>'
 
 threads 1, 6
 # I'm too tight to pay for any more than a single-core server
@@ -113,8 +113,15 @@ sudo ln -s \
 sudo sh -c "echo $ROOT_PATH >> /etc/puma/puma.conf"
 
 # Create database role & blank database for the app
-sudo -u postgres createuser $APP_NAME
-sudo -u postgres createdb $APP_NAME
+# Assumes runner of this script is already setup as a postgres superuser
+createuser $APP_NAME
+createdb $APP_NAME -O $APP_NAME
+psql -d $APP_NAME -c "GRANT pg_read_all_data, pg_write_all_data TO $APP_NAME;"
+
+echo "Now add a password for the app database:"
+echo "1) Start interactive postrgres shell: '$ psql'"
+echo -e "2) Type: '\\password $APP_NAME'"
+echo "3) Record the password in your app settings e.g. config/database.yml"
 
 # Restart Nginx & Puma
 sudo service puma-manager restart
--- ruby/wipe-app.sh
+++ ruby/wipe-app.sh
@@ -6,3 +6,12 @@ sudo sh -c "rm -f /etc/nginx/sites-enabled/$APP_NAME.conf"
 sudo sh -c "rm -f /etc/nginx/sites-available/$APP_NAME.conf"
 sudo sh -c "rm -rf /var/www/$APP_NAME"
 sudo sh -c "sed -i '/$APP_NAME/d' /etc/puma/puma.conf"
+
+echo -n "Delete associated database? (y/n)"
+read RES
+
+if [[ $RES == "y" ]]; then
+  psql -c "DROP DATABASE IF EXISTS $APP_NAME;"
+  psql -c "DROP USER IF EXISTS $APP_NAME;"
+fi
+
