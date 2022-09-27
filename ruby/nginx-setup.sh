11fe53e26a007607b50e661b4a3685043bd94efb
James MacAdie (server name) <james@macadie.co.uk>
2022-09-27 16:36:18 +0100

    Add certbot
    It won't work it it's not installed :)

--- ruby/nginx-setup.sh
+++ ruby/nginx-setup.sh
@@ -2,5 +2,9 @@
 
 sudo apt-get install nginx
 
+# install certbot
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
 sudo service nginx start
-systemctl status nginx.service
+sudo systemctl status nginx.service
